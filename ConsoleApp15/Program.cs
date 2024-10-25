7c677c12e65399ccd11440543bba1dd28c01ddef
Folly37 <follochka11@gmail.com>
2024-10-25 11:33:44 +0500

    создание функции FindElement

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -10,11 +10,6 @@ namespace ConsoleApp15
     {
         static void Main(string[] args)
         {
-
-            int Fib = Fibonachi(4);
-            Console.WriteLine(Fib);
-            int Akk = Akkerman(1,6);
-            Console.WriteLine(Akk);
             int[,] array = {
             { 1, 2, 3 },
             { 4, 5, 6 },
@@ -23,17 +18,6 @@ namespace ConsoleApp15
             int target = 5;
             var result = FindElement(array, target, 0, 0);
             Console.WriteLine(result);
-        }
-        static int Fibonachi(int n)
-        {
-            if (n == 0 || n == 1) return n;
-            return Fibonachi(n - 1) + Fibonachi(n - 2);
-        }
-        static int Akkerman(int n, int m)
-        {
-            if (n == 0) return m + 1;
-            else if ((n != 0) && (m == 0)) return Akkerman(n - 1, 1);
-            else return Akkerman(n - 1, Akkerman(n, m - 1));
         }
         static (int, int) FindElement(int[,] array, int target, int row, int col)
         {
